22d56afda60ae8a658dae1107e9287059720e74b
Cawolf <camilozambranovotto@hotmail.com>
2023-12-18 15:05:50 -0500

    Added basic node test skeleton :+1:

---
+++ tests/server.test.js
@@ -0,0 +1,8 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+
+describe("Test mientras WIP", () => {
+	it("WIP test", () => {
+		assert.strictEqual(1, 1);
+	});
+});
