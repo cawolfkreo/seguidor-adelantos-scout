955abe31bc05b0beea0d0be31200d121b81f0249
Cawolf <camilozambranovotto@hotmail.com>
2023-12-18 18:00:03 -0500

    Added Swagger to the project :godmode:

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 "use strict";
 
 import express from "express";
+
+import { RUN_IN_DEV } from "./src/setup.js";
+import setupSwagger from "./src/swagger-setup.js";
+
 const app = express();
 const port = 9000;
 
@@ -8,10 +12,22 @@ console.log("WIP - Todo: Everything!!!");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+/**
+* @openapi
+* /hello:
+*   get:
+*     description: Welcome to swagger-jsdoc!
+*     responses:
+*       200:
+*         description: Returns a mysterious string.
+*/
+app.get("/hello", (_req, res) => {
 	res.send("Hola mundo");
 });
 
 app.listen(port, () => {
 	console.log(`Express listening on port ${port}`);
+	if (RUN_IN_DEV) {
+		setupSwagger(app, port);
+	}
 });
